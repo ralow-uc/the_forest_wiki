ec0089d3ec74b3c647add19e696913d131cc59d4
Raul Low <raulou92@gmail.com>
2025-04-12 14:13:29 -0400

    feat: db connection

--- the_forest_wiki/settings.py
+++ the_forest_wiki/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'the_forest_wiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/FREEPDB1',
+        'USER': 'system',
+        'PASSWORD': 'Ora1234',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
 TIME_ZONE = 'UTC'
 
